189b5ea2baa3268e86a7744ca1715cde9b420363
HannahYiCodes <hannahyi.students@careerdev.com>
2022-06-16 20:03:25 -0400

    initial commit

---
+++ src/com/careerdevs/Example.java
@@ -0,0 +1,21 @@
+// example of getters/setters
+package com.careerdevs;
+
+public class Example {
+
+    private String name;
+    private int age;
+
+    public Example (String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName () {
+     return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
---
+++ src/com/careerdevs/Main.java
@@ -0,0 +1,10 @@
+package com.careerdevs;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Example myExample = new Example("Bill", 40);
+        System.out.println( myExample.getName());
+
+    }
+}
